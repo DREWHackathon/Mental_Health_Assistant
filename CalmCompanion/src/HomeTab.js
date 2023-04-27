1a3a0bb32d9b20161dd83c4a46bac4b189c1090c
Roger Li <rel7817@nyu.edu>
2023-04-26 20:44:18 -0400

    font size change

--- CalmCompanion/src/HomeTab.js
+++ CalmCompanion/src/HomeTab.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
       marginBottom: 1,
     },
     textTitle: {
-      fontSize: 45,
+      fontSize: 30,
       fontWeight: 800,
       marginLeft: 15,
       marginRight: 15,
